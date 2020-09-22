64b84e72e2ed4ef7870098684df38b876298ba9a
convivial-parser-7775 <avelonpang@gmail.com>
2020-09-22 06:34:29 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -6,3 +6,13 @@ new_array.push(array[count] ** 2)
 end
 new_array
 end
+
+def square_array(array)
+
+ new_array = []
+
+ array.each do |integer|
+  new_array << integer ** 2
+ end
+ return new_array
+end
