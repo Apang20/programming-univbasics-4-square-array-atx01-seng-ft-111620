c255409dc30fd93d7a0a02728f9de740d03ac15e
convivial-parser-7775 <avelonpang@gmail.com>
2020-09-22 06:34:57 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,11 +1,11 @@
-def square_array(array)
-  count = 0 
-  new_array = []
- while count < array.length do 
-new_array.push(array[count] ** 2)
-end
-new_array
-end
+#def square_array(array)
+  #count = 0 
+  #new_array = []
+ #while count < array.length do 
+#new_array.push(array[count] ** 2)
+#end
+#new_array
+#end
 
 def square_array(array)
 
